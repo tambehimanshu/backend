c35253b2e4abe734e9ff55c60d23a28035089307
tambehimanshu <tambehimanshu18@gmail.com>
2025-02-25 12:08:18 +0530

    use of filter method

--- basic/script.js
+++ basic/script.js
@@ -2,4 +2,10 @@ var arr=[1,2,3,4];
 var ans = arr.map(function(val){
     return val*2;
 })
-console.log(ans);
+var wr = arr.filter(function(val){
+    if(val>2){
+        return true;
+    }
+    else return false;
+})
+console.log(wr);
